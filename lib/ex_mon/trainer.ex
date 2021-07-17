a86b7cbaace307e5963d26e89a28978190bc2e66
kakuzops <kakuzops@gmail.com>
2021-07-16 21:41:26 -0300

    method build in created

--- lib/ex_mon/trainer.ex
+++ lib/ex_mon/trainer.ex
@@ -7,20 +7,37 @@ defmodule ExMon.Trainer do
 
   @required [
     :name,
-    :password_hash
+    :password
   ]
 
 
   schema "trainers" do
     field :name, :string
-    field :password_hash, :string
+    field :password_hash
+    field :password, :string, virtual: true
 
     timestamps()
   end
+
+
 
   def changeset(params) do
     %__MODULE__{}
     |> cast(params, @required)
     |> validate_required(@required)
+    |> validate_length(:password, min: 6)
+    |> put_pass_hash()
   end
+
+   def build(params) do
+    params
+    |> changeset()
+    |> apply_action(:insert)
+  end
+
+  defp put_pass_hash(%Ecto.Changeset{valid?: true, changes: %{password: password}} = changeset) do
+    change(changeset, Argon2.add_hash(password))
+  end
+
+  defp put_pass_hash(changeset), do: changeset
 end
